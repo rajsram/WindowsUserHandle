5a71662a34ca4e5b1c14c7fdd9e30ea8f05be232
rajsram <RajeshSivasankaran@outlook.com>
2024-07-26 16:50:43 +0530

    Solution Added

---
+++ UserHandle/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace UserHandle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter User Account Options 1 to create and 2 to remove and 3 to reset password:");
+                var op= Console.ReadLine();
+                Console.WriteLine("Enter user name:");
+                var un = Console.ReadLine();
+                if (op == "1")
+                {
+                    Console.WriteLine("Enter Password:");
+                    var pwd = Console.ReadLine();
+                    DirectoryEntry AD = new DirectoryEntry("WinNT://" +
+                    Environment.MachineName + ",computer");
+                    DirectoryEntry NewUser = AD.Children.Add(un, "user");
+                    NewUser.Invoke("SetPassword", new object[] { pwd });
+                    NewUser.Invoke("Put", new object[] { "Description", "Test User from .NET" });
+                    NewUser.CommitChanges();
+                    DirectoryEntry grp;
+
+                    grp = AD.Children.Find("Guests", "group");
+                    if (grp != null) { grp.Invoke("Add", new object[] { NewUser.Path.ToString() }); }
+                    Console.WriteLine("Account Created Successfully");
+                }
+                else
+                {
+                    DirectoryEntry localDirectory = new DirectoryEntry("WinNT://" + Environment.MachineName.ToString());
+                    DirectoryEntries users = localDirectory.Children;
+                    DirectoryEntry user = users.Find(un);
+                    if (op == "2")
+                    {
+                        users.Remove(user);
+                        Console.WriteLine("Account Removed Successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter new Password:");
+                        var pwd = Console.ReadLine();
+                        user.Invoke("SetPassword", new object[] { pwd });
+                        user.CommitChanges();
+                        Console.WriteLine("Account Password Changed Successfully");
+                    }
+                   
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ UserHandle/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UserHandle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Carl Zeiss AG")]
+[assembly: AssemblyProduct("UserHandle")]
+[assembly: AssemblyCopyright("Copyright © Carl Zeiss AG 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("107ba066-2c4d-41a6-b644-841f1ef641da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
